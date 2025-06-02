ce214eb4cff866b37bc5fd955aaa1aa3e869f201
Neisser Arilson Moreno Sanchez <neisser.moreno@hassperu.com>
2025-06-02 08:31:09 -0500

    envio de correos

--- job/recordatorioFirma.js
+++ job/recordatorioFirma.js
@@ -23,8 +23,8 @@ const transporter = nodemailer.createTransport({
 
 const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-cron.schedule("0 9 * * *", async () => {
-  // cron.schedule("* * * * *", async () => {
+// cron.schedule("0 9 * * *", async () => {
+  cron.schedule("* * * * *", async () => {
   // QUE SE ENVIE CADA MINUTO
   console.log("🔁 Ejecutando tarea programada de recordatorios de firma");
 
@@ -45,7 +45,7 @@ cron.schedule("0 9 * * *", async () => {
         (hoy - fechaCreacion) / (1000 * 60 * 60 * 24)
       );
 
-      if (diasPasados < 5) {
+      // if (diasPasados < 5) {
         const correos = [
           solicitud.correoSolicitante,
           solicitud.correoConyuge,
@@ -59,7 +59,7 @@ cron.schedule("0 9 * * *", async () => {
           });
           console.log(`✉️ Correo enviado a: ${correo}`);
         }
-      }
+      // }
 
       if (diasPasados >= 5) {
         // if (true) {
